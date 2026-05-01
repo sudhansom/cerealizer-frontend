8ce1682b30800f7ca4d2c8d6721e035e44aeec6f
Bhuwan Krishna Som Poudel <bkspoudel@gmail.com>
2026-05-01 09:18:11 +0200

    create a reactive form to take the values

--- src/app/components/display-cereals/display-cereals.ts
+++ src/app/components/display-cereals/display-cereals.ts
@@ -1,6 +1,7 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { ICereal } from '../../models/cereal-types';
 import { CerealService } from '../../services/cereal-service';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 interface User {
   id: number;
@@ -12,13 +13,15 @@ interface User {
 
 @Component({
   selector: 'app-display-cereals',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './display-cereals.html',
   styleUrl: './display-cereals.css',
 })
-export class DisplayCereals {
+export class DisplayCereals implements OnInit {
   @Input() cereals!: ICereal[];
+  cerealForm!: FormGroup;
   cerealService = inject(CerealService);
+  addNewCereal = false;
   isFocused = false;
   titles = [
     'S.N',
@@ -28,6 +31,7 @@ export class DisplayCereals {
     'Calories',
     'Protein',
     'Sodium',
+    'Fiber',
     'Fat',
     'Sugar',
     'Potass',
@@ -45,6 +49,7 @@ export class DisplayCereals {
     'calories',
     'protein',
     'sodium',
+    'fiber',
     'fat',
     'sugar',
     'potass',
@@ -56,10 +61,36 @@ export class DisplayCereals {
     'image',
   ];
 
+  ngOnInit(): void {
+    this.cerealForm = new FormGroup({
+      name: new FormControl('dfdsfds', Validators.required),
+      mfr: new FormControl('', Validators.required),
+      type: new FormControl('', Validators.required),
+      calories: new FormControl(0, Validators.required),
+      protein: new FormControl(0, Validators.required),
+      sodium: new FormControl(0, Validators.required),
+      fiber: new FormControl(0, Validators.required),
+      fat: new FormControl(0, Validators.required),
+      potass: new FormControl(0, Validators.required),
+      vitamins: new FormControl(0, Validators.required),
+      self: new FormControl(0, Validators.required),
+      cups: new FormControl(0, Validators.required),
+      rating: new FormControl(0, Validators.required),
+      image: new FormControl(0, Validators.required),
+    });
+  }
+
   updateCereal(event: any, id: string, titleValue: string) {
     console.log(event.target.value, id, titleValue);
     this.cerealService.updateCereal(id, titleValue, event.target.value).subscribe((cereal) => {
       console.log(cereal);
     });
+  }
+
+  addCereal() {
+    if (this.addNewCereal) {
+      alert('Saved');
+    }
+    this.addNewCereal = !this.addNewCereal;
   }
 }
